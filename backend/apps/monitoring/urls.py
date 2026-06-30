13116952b8e37cf1710f8b85131f14ecd1d5a961
asliddintursunoff <asliddintursunoff3@gmail.com>
2026-06-30 13:07:45 +0500

    healthchekAPI fix

--- backend/apps/monitoring/urls.py
+++ backend/apps/monitoring/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from apps.monitoring.views import HealthCheckAPIView
 
 urlpatterns = [
-    path('health/',HealthCheckAPIView.as_view())
+    path('health/',HealthCheckAPIView.as_view() )
 ]
